28aeb0cc691c4af01c350bcc375e8b07a200a677
juliswer <julianswer36@gmail.com>
2021-02-05 00:19:38 -0300

    Acá se añade lo aprendido en react

---
+++ Estudio/react-s-proyect.-master/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  const contadorInicial = Number(localStorage.getItem('contador'))
+  const [counter, setCounter] = React.useState(contadorInicial);
+
+  const sumarContador = () => {
+    setCounter(counter + 1);
+  }
+
+  React.useEffect(() => {
+    if (counter !== 0) {
+      localStorage.setItem('contador', counter);
+    }
+  }, [counter]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Challenge <code> react </code>
+        </p>
+        <button
+          onClick={sumarContador}
+        >
+          No hagas click aca
+        </button>
+        <p>
+          {counter}
+        </p>
+      </header>
+    </div>
+  );
+}
