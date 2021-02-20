2eb5b5fb95ab0919a3fb2df9714ccddbc067122a
Julián Swerdlin <julianswer36@gmail.com>
2021-02-20 19:49:26 -0300

    Updated. (Clase 21)

--- Estudio/Javascript/Curso_codigo_facilito.js
+++ Estudio/Javascript/Curso_codigo_facilito.js
@@ -608,3 +608,36 @@ console.log(nombre.toLowerCase()); //vuelve todo a minúscula
   var pepito = new tutor ("Pepito");
 
   pepito.saludar();
+
+                        'CLASE 21'
+
+  function tutor () {
+    this.nombre = nombre;
+    this.saludar = function() {
+      console.log("Hola a todos, soy " + this.nombre);
+    }
+    if(typeof apellido !== "undefined"){
+    this.nombre_completo = nombre + " " + apellido;
+    }
+
+  }               
+
+  Tutor.prototype.edad = 22;
+  
+  var julian = new tutor("Julián", "Swerdlin");
+  var jose = new tutor("Jose");
+
+  julian.iniciar_tutorial = function(){
+    console.log("Bienvenidos a este nuevo tutorial")
+  }
+
+  julian.iniciar_tutorial();
+
+  julian.edad = 16;
+
+  console.log(julian.edad);
+  console.log(jose.edad); //Esto nos devuelve undefined, aunque luego del prototype de tutor, va a pasar a devolver 22.
+
+  jose.iniciar_tutorial()
+
+  // En resumen la propiedad prototype sirve para darle un valor a todos los elementos. Por ejemplo como vimos con la edad de los tutores  
