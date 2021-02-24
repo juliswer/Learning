e4cec51e52c7a2caf9ee142b1894a729a9582086
Julián Swerdlin <julianswer36@gmail.com>
2021-02-24 16:51:03 -0300

    I made a galeria slider with glider.js

---
+++ Estudio/Diseño_web/FalconMasters/glider.js/glider.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  breakpoint: 450,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
