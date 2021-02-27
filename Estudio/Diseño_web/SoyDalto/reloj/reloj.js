038b673eb8031c4365b0ce1e9ef9cfa7b16b8a6f
Julián Swerdlin <julianswer36@gmail.com>
2021-02-27 09:43:08 -0300

    I made a watch

---
+++ Estudio/Diseño_web/SoyDalto/reloj/reloj.js
@@ -0,0 +1,24 @@
+function reloj() {
+    time = new Date();
+    horas = time.getHours();
+    minutos = time.getMinutes();
+    segundos = time.getSeconds();
+
+    if (horas >= 12){
+        porcentajeHoras = horas / 12 * 360;
+    } else {
+        porcentajeHoras = horas / 24 * 360;
+    }
+
+    porcentajeHoras += minutos / 60 * 30;
+    porcentajeMinutos = minutos / 60 * 360;
+    porcentajeSegundos = segundos / 60 * 360;
+
+    document.getElementById('horas').style.transform = "rotate("+ porcentajeHoras +"deg)";
+    document.getElementById('minutos').style.transform = "rotate("+ porcentajeHoras +"deg)";
+    document.getElementById('segundos').style.transform = "rotate("+ porcentajeHoras +"deg)";
+    document.getElementById('h4-content').innerHTML = "Hora actual";
+    document.getElementById('p-content').innerHTML = horas + ":" + minutos + ":" + segundos;
+}
+
+setInterval(reloj, 1000);
