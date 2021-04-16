3a316d95ea4c37f85d9a081719d7e32edca4a088
Julián Swerdlin <julianswer36@gmail.com>
2021-04-15 21:23:12 -0300

    Add files via upload

--- Estudio/Diseño_web/4tomik/Javascript/js/model.js
+++ Estudio/Diseño_web/4tomik/Javascript/js/model.js
@@ -1,18 +1,62 @@
-class Model {
+export default class Model {
     constructor(){
         this.view = null;
-        this.todos = []
+        this.todos = JSON.parse(localStorage.getItem('todos'));
+        if (!this.todos || this.todos.length < 1){
+            this.todos = [
+                {
+                    id: 0,
+                    title: 'Learn JS',
+                    description: 'Watch JS Tutorials',
+                    completed: false
+                }
+            ]
+            this.currentId = 1;
+        } else {
+            this.currentId = this.todos[this.todos.length - 1].id + 1;
+        }
     }
 
     setView(view) {
         this.view = view;
     }
 
+    save() {
+        localStorage.setItem('todos', JSON.stringify(this.todos));    
+    }
+
     getTodos() {
         return this.todos;
     }
 
+    findTodo(id){
+        return this.todos.findIndex((todo) => todo.id === id);
+    }
+
+    toggleCompleted(id){
+        const index = this.findTodo(id);
+        const todo = this.todos[index];
+        todo.completed = !todo.completed;
+        console.log(this.todos);
+    }
+
     addTodo(title, description) {
-        console.log(title,description);
+        const todo = {
+            id: this.currentId++,
+            title,
+            description,
+            completed: false,
+        }
+
+        this.todos.push(todo);
+        console.log(this.todos);
+        this.save();
+        return {...todo};
+    }
+
+    removeTodo(id){
+        const index = this.findTodo(id); 
+        this.todos.splice(index, 1);
+        this.save();
     }
 }
